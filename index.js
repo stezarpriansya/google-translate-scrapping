c5d0e7bc020d496658a29b4492a5c1be0956538c
Stezar Priansya <stezarpriansya@gmail.com>
2018-09-17 16:26:15 +0700

    updated frontend

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+	.get('/submit-translate', function(request,response){
+			var query = request.query.word;
+			var from = 'id';
+			var to = 'en';
+			if(request.query.from){
+				form = request.query.from
+			}
+			if(request.query.to){
+				to = request.query.to
+			}
+			translate(query, {from: from, to: to}).then(res => {
+				var isTranslated = false;
+				var initialText = query;
+			    var translatedText = res.text.toLowerCase();
+			    if(translatedText !== initialText){
+			    	isTranslated = true;
+			    }
+			    var autoCorrected = res.from.text.autoCorrected;
+			    var correctedText = res.from.text.value.toLowerCase().replace(/[^\w\s]/gi, '');
+			    var didYouMean = res.from.text.didYouMean;
+
+					response.render('pages/index', {initialText: initialText, translatedText: translatedText, isTranslated: isTranslated, autoCorrected: autoCorrected, correctedText: correctedText, didYouMean:didYouMean})
+
+			}).catch(err => {
+			    console.error(err);
+			});
+	})
 	.get('/translate', function(request,response){
 		var query = request.query.word;
 		var from = 'id';
