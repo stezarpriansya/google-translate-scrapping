3f6a465e497da8c29b7ed0e803f30a27af093e8d
Stezar Priansya <stezarpriansya@gmail.com>
2018-09-17 16:54:26 +0700

    perbaikan index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ express()
 	.use(bodyParser.urlencoded({ extended: false }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index', {}))
+  .get('/', (req, res) => res.render('pages/index', {initialText: '', translatedText: ''}))
 	.get('/submit-translate', function(request,response){
 			var query = request.query.word;
 			var from = 'id';
